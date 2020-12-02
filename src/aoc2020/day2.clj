ab3a3b6a45e310ab306c3afccab861615c24a4b6
Daniel Rosowski <rosowski@smartsquare.de>
2020-12-02 15:32:26 +0100

    day2

---
+++ src/aoc2020/day2.clj
@@ -0,0 +1,22 @@
+(ns aoc2020.day2)
+
+(defn- check-pw [min max ch pw]
+  (let [f ((frequencies pw) ch)]
+    (<= min (if (nil? f) 0 f) max)))
+
+(defn parse-args [s]
+  (re-matches #"(\d+)-(\d+)\s([a-z]):\s([a-z]+)" s))
+
+(defn- valid-pw-p1? [input]
+  (let [[_ min max ch pw] (parse-args input)]
+    (check-pw (Integer. min) (Integer. max) (.charAt ch 0) pw)))
+
+(defn count-valid-pws-p1 [pws]
+  (count (filter valid-pw-p1? (clojure.string/split-lines pws))))
+
+(defn valid-pw-p2? [input]
+    (let [[_ pos1 pos2 ch pw] (parse-args input)]
+    (= 1 (count (filter #(= (.charAt ch 0) %) (str (nth pw (dec (Integer. pos1))) (nth pw (dec (Integer. pos2)))))))))
+
+(defn count-valid-pws-p2 [pws]
+  (count (filter valid-pw-p2? (clojure.string/split-lines pws))))
